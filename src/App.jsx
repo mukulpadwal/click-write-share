46a7051f97b78d23345ac2e2a6e6640fa4d2b02d
mukulpadwal <mukulpadwal786@gmail.com>
2024-02-02 21:51:57 +0530

    FEAT : redux-toolkit-store-integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import {Header, Footer} from "./components"
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import login, { logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_REACT_PUBLIC_APP_APPWRITE_URL);
-  return <div className="h-screen w-full bg-black text-white">{import.meta.env.VITE_REACT_PUBLIC_APP_APPWRITE_URL}</div>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData} ));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
